067a699d3dc628f13be8f165b28cda84cbebe984
Bishal Roy <bishalroy15jan@gmail.com>
2024-04-30 02:41:42 +0530

    Enhanced Pricing Component

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { Heading, PricingLists, Section } from './supports'
 import { smallSphere, stars } from '../assets'
 import { LeftLine, RightLine } from './design/Pricing'
+import { ScrollParallax } from 'react-just-parallax'
 
 const Pricing = () => {
     return (
         <Section className='overflow-hidden' id='pricing'>
             <div className="container relative z-2">
                 <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
-                    <img
-                        src={smallSphere}
-                        className='relative z-1'
-                        width={255}
-                        height={255}
-                        alt="Sphere"
-                    />
+                    <ScrollParallax shouldPause strength={0.01}>
+                        <img
+                            src={smallSphere}
+                            className='relative z-1 mx-auto'
+                            width={255}
+                            height={255}
+                            alt="Sphere"
+                        />
+                    </ScrollParallax>
 
                     <div className='absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none'>
                         <img
@@ -22,7 +25,7 @@ const Pricing = () => {
                             alt="stars"
                             width={950}
                             height={400}
-                            className='w-full'
+                            className='w-full animate-pulse'
                         />
                     </div>
                 </div>
@@ -40,7 +43,7 @@ const Pricing = () => {
                 </div>
 
                 <div className='flex justify-center mt-10'>
-                    <a href="/pricing" className='text-xs font-code font-bold tracking-wider border-b'>See the full details</a>
+                    <a href="/pricing" className='text-xs md:text-sm font-code font-bold tracking-wider border-b'>See the full details</a>
                 </div>
             </div>
 
