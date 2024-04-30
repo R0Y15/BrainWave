5d4ebf8f64b5d13ad49cf00d3cc349cbb5a284b2
Bishal Roy <bishalroy15jan@gmail.com>
2024-04-30 12:20:48 +0530

    footer created

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import Collaboration from "./Collaboration";
 import Services from "./Services";
 import Pricing from "./Pricing";
 import Roadmap from "./Roadmap";
+import Footer from "./Footer";
 
 export {
     Header,
@@ -14,4 +15,5 @@ export {
     Services,
     Pricing,
     Roadmap,
+    Footer
 }
